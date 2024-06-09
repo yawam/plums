15dec500e9883ded1074a89f33068cb4c05b54be
aj-et <tumbokon.aaronjulius@outlook.com>
2024-06-09 00:04:59 -0600

    moved files

--- src/components/ui/file-upload.tsx
+++ src/components/ui/file-upload.tsx
@@ -2,7 +2,7 @@
 
 import toast from "react-hot-toast";
 import { UploadDropzone } from "@/lib/uploadthing";
-import { ourFileRouter } from "@/app/api/uploadthing/core";
+import { ourFileRouter } from "@/app/[topicId]/api/uploadthing/core";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
