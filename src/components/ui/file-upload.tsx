99051ddb79064eb24fe73491799353bf170c65e2
aj-et <tumbokon.aaronjulius@outlook.com>
2024-06-09 00:06:14 -0600

    moved api out

--- src/components/ui/file-upload.tsx
+++ src/components/ui/file-upload.tsx
@@ -2,7 +2,7 @@
 
 import toast from "react-hot-toast";
 import { UploadDropzone } from "@/lib/uploadthing";
-import { ourFileRouter } from "@/app/[topicId]/api/uploadthing/core";
+import { ourFileRouter } from "@/api/uploadthing/core";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
