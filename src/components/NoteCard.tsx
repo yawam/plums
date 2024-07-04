87e63fa6c216fd3bf1160c1d902e53b510c77a34
aj-et <tumbokon.aaronjulius@outlook.com>
2024-07-04 16:31:14 -0600

    fixed the card being on the top when modal is open

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -13,10 +13,10 @@ const NoteCard = ({ note_id, title, note }: NoteCardProps) => {
   const handleDelete = () => {};
   return (
     <div
-      className="rounded-2xl bg-fuchsia-900 text-white h-[180px] shadow-xl p-4 relative cursor-pointer"
+      className="rounded-2xl bg-fuchsia-900 text-white h-[180px] shadow-xl p-4 relative cursor-pointer z-20"
       key={note_id}
     >
-      <div className="absolute top-2 right-2 cursor-pointer transition hover:bg-black hover:bg-opacity-50 p-2 shadow-xl rounded-xl">
+      <div className="absolute top-2 right-2 cursor-pointer transition hover:bg-black hover:bg-opacity-50 p-2 shadow-xl rounded-xl z-20">
         <BsTrash
           size={25}
           onClick={() => {}} // a delete by note id. remember to add to recently deleted for all delete functions
