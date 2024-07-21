6eb024e66708d448a6559a2c3671ed3ca4f5175d
aj-et <tumbokon.aaronjulius@outlook.com>
2024-07-20 22:13:03 -0600

    fixed errors and warnings

--- src/pages/NewTopicButton.tsx
+++ src/pages/NewTopicButton.tsx
@@ -2,8 +2,9 @@
 
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { AlertDialog, AlertDialogContent, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { AlertDialog, AlertDialogContent, AlertDialogTrigger, AlertDialogTitle, AlertDialogDescription } from "@/components/ui/alert-dialog";
 import NewNewTopicModal from "./NewNewTopicModal";
+import VisuallyHidden from "@/components/ui/visually-hidden";
 import "remixicon/fonts/remixicon.css";
 
 interface NewTopicButtonProps {
@@ -24,6 +25,10 @@ const NewTopicButton = ({ topicId }: NewTopicButtonProps ) => {
             </Button>
         </AlertDialogTrigger>
         <AlertDialogContent className="p-0 border-none bg-transparent m-0 shadow-none">
+            <VisuallyHidden>
+                <AlertDialogTitle>New Sub-Topic</AlertDialogTitle>
+                <AlertDialogDescription>This creates a new sub-topic</AlertDialogDescription>
+            </VisuallyHidden>
             <NewNewTopicModal closeModal={() => setIsOpen(false)} parentID={topicId} />
         </AlertDialogContent>
         </AlertDialog>
