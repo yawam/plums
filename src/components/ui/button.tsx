6970f7dc1edf77536d145432284deb4e7f5340f3
Papa Yaw Agyeman-Manu <agy20002@byui.edu>
2024-07-21 00:10:10 -0600

    button css

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-fuchsia-900 hover:bg-fuchsia-900 hover:text-emerald-300 rounded-xl shadow-xl hover:shadow-emerald-300",
+          "border border-fuchsia-900 hover:bg-fuchsia-900 hover:text-emerald-500 rounded-xl shadow-xl hover:shadow-emerald-500",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
